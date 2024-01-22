5fca162050883af33dd3800b47793c724db56ccf
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2024-01-22 16:32:33 +0400

    setting up the page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { UsertableComponent } from './usertable/usertable.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: 'users', component: UsersComponent },
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { UsertableComponent } from '../usertable/usertable.component';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    UsertableComponent
+  ]
+})
+export class UsersComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Users', cols: 2, rows: 1 },
+
+        ];
+      }
+
+      return [
+        { title: 'Users', cols: 2, rows: 1 },
+
+      ];
+    })
+  );
+}
