85575e253542be42b0cd517ed1d60c3c8acfe085
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2024-01-24 15:12:18 +0400

    adding schematics and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { userReducer } from './core/reducers/user.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), provideStore(), provideState('users', userReducer), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), provideStore(), provideState('users', userReducer), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideEffects()]
 };
